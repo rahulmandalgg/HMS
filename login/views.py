cc1f3c9a731d5b6a494f914ed65c61db4a09fc65
Rahul <lachrymoseeee@gmail.com>
2023-03-02 00:23:47 +0530

    go to setting.py and enter your password in database dictonary.

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 import mysql.connector as sql
-from HMS.settings import DATABASES
+from django.views.decorators.csrf import csrf_protect
+
 
 # Create your views here.
 
@@ -9,11 +10,10 @@ pas=''
 usrtype= ''
 
 m=sql.connect(host="localhost",user="root",passwd="mandal.1234",database="hospital")
-
+c=m.cursor()
 def login(request):
-    global id,pas,usrtype,m
+    global id,pas,usrtype,m,c
     if request.method=="POST":
-        c=m.cursor()
         d=request.POST
         for key,value in d.items():
             if key=='EmployeeID':
@@ -43,14 +43,38 @@ def login(request):
                 return render(request,'login.html',{'msg':'Invalid Credentials'})
     return render(request, 'login.html')
 
+@csrf_protect
 def admin(request):
-
 
     c = m.cursor()
     c.execute("select * from users")
     results = c.fetchall()
-    #print(results)
     result = {"staff": results}
+
+
+
+    if request.method == 'POST' and request.POST.get("form_type") == 'rform':
+        remove_id = request.POST.get("remove-user-id")
+        c.execute("delete from users where EmployeeID='"+remove_id+"'")
+        m.commit()
+        return render(request,'admin.html', {'delmsg': 'User Deleted Successfully'})
+
+    if request.method == 'POST' and request.POST.get("form_type") == 'addform':
+        user_type = request.POST.get("type")
+        first_name = request.POST.get("first-name")
+        last_name = request.POST.get("last-name")
+        employee_id = request.POST.get("employee-id")
+        password = request.POST.get("password")
+
+        if user_type == 'doctor':
+            specialization = request.POST.get("specialization")
+            cert_given_date = request.POST.get("certificate-given-date")
+            cert_expiry_date = request.POST.get("certificate-expiry-date")
+            add_user_to_database(user_type, first_name, last_name, employee_id, password, specialization, cert_given_date,
+                                 cert_expiry_date)
+        else:
+            add_user_to_database(user_type, first_name, last_name, employee_id, password)
+
     return render(request,'admin.html',result)
 
 def doctor(request):
@@ -61,3 +85,26 @@ def frontdesk(request):
 
 def dataoperator(request):
     return render(request,'data_operator.html')
+
+def add_user_to_database(user_type, first_name, last_name, employee_id, password,specialization=None, cert_given_date=None, cert_expiry_date=None):
+    # Define the MySQL query
+    if user_type == 'doctor':
+        query = """INSERT INTO users (Type, First_Name, Last_Name, EmployeeID, Password, Specialization, CertificateGD, CertificateED)
+                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        values = (user_type, first_name, last_name, employee_id, password, specialization, cert_given_date, cert_expiry_date)
+    else:
+        query = """INSERT INTO users (Type, First_Name, Last_Name, EmployeeID, Password)
+                   VALUES (%s, %s, %s, %s, %s)"""
+        values = (user_type, first_name, last_name, employee_id, password)
+    # Execute the query and commit the changes
+    c.execute(query, values)
+    m.commit()
+
+def delete_user_from_database(employee_id):
+    # Define the MySQL query
+    query = """DELETE FROM users WHERE EmployeeID = %s"""
+    values = (employee_id)
+
+    # Execute the query and commit the changes
+    c.execute(query, values)
+    m.commit()
